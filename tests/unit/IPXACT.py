e4ba452f0ffc4ae7f177fead90f000599e5ceba9
Patrick Lehmann <Paebbels@gmail.com>
2023-07-19 20:36:16 +0200

    Improved schema testing (sanity checking).

--- tests/unit/IPXACT.py
+++ tests/unit/IPXACT.py
@@ -10,28 +10,113 @@ if __name__ == "__main__":
 
 
 class SPIRIT(TestCase):
+	_root: Path = Path("../..")
+
 	def test_Schema10(self):
-		schema = XMLSchema(Path("../../ipxact-1.0/design.xsd"))
+		directory = self._root / "ipxact-1.0"
+
+		schemaFiles = (
+			"design.xsd",
+			"pmd.xsd",
+			"component.xsd",
+			"busDefinition.xsd",
+			"generator.xsd",
+			"configurator.xsd",
+			"looseGeneratorInvocation.xsd",
+		)
+		for schemaFile in schemaFiles:
+			_ = XMLSchema(directory / schemaFile)
 
 	def test_Schema11(self):
-		schema = XMLSchema(Path("../../ipxact-1.1/design.xsd"))
+		directory = self._root / "ipxact-1.1"
+
+		schemaFiles = (
+			"design.xsd",
+			"pmd.xsd",
+			"component.xsd",
+			"busDefinition.xsd",
+			"generator.xsd",
+			"configurator.xsd",
+			"looseGeneratorInvocation.xsd",
+		)
+		for schemaFile in schemaFiles:
+			_ = XMLSchema(directory / schemaFile)
 
 	def test_Schema12(self):
-		schema = XMLSchema(Path("../../ipxact-1.2/design.xsd"))
+		directory = self._root / "ipxact-1.2"
+
+		schemaFiles = (
+			"design.xsd",
+			"pmd.xsd",
+			"component.xsd",
+			"busDefinition.xsd",
+			"generator.xsd",
+			"configurator.xsd",
+			"looseGeneratorInvocation.xsd",
+		)
+		for schemaFile in schemaFiles:
+			_ = XMLSchema(directory / schemaFile)
 
 	def test_Schema14(self):
-		schema = XMLSchema(Path("../../ipxact-1.4/design.xsd"))
+		directory = self._root / "ipxact-1.4"
+
+		schemaFiles = (
+			"design.xsd",
+			"component.xsd",
+			"busDefinition.xsd",
+			"generator.xsd",
+		)
+		for schemaFile in schemaFiles:
+			_ = XMLSchema(directory / schemaFile)
 
 	def test_Schema15(self):
-		schema = XMLSchema(Path("../../ipxact-1.5/design.xsd"))
+		directory = self._root / "ipxact-1.5"
+
+		schemaFiles = (
+			"design.xsd",
+			"component.xsd",
+			"busDefinition.xsd",
+			"generator.xsd",
+		)
+		for schemaFile in schemaFiles:
+			_ = XMLSchema(directory / schemaFile)
 
 
 class IEEE_1685(TestCase):
+	_root: Path = Path("../..")
+	
 	def test_Schema2009(self):
-		schema = XMLSchema(Path("../../ieee-1685-2009/design.xsd"))
+		directory = self._root / "ieee-1685-2009"
+
+		schemaFiles = (
+			"design.xsd",
+			"component.xsd",
+			"busDefinition.xsd",
+			"generator.xsd",
+		)
+		for schemaFile in schemaFiles:
+			_ = XMLSchema(directory / schemaFile)
 
 	def test_Schema2014(self):
-		schema = XMLSchema(Path("../../ieee-1685-2014/design.xsd"))
+		directory = self._root / "ieee-1685-2014"
+
+		schemaFiles = (
+			"design.xsd",
+			"component.xsd",
+			"busDefinition.xsd",
+			"generator.xsd",
+		)
+		for schemaFile in schemaFiles:
+			_ = XMLSchema(directory / schemaFile)
 
 	def test_Schema2022(self):
-		schema = XMLSchema(Path("../../ieee-1685-2022/design.xsd"))
+		directory = self._root / "ieee-1685-2022"
+
+		schemaFiles = (
+			"design.xsd",
+			"component.xsd",
+			"busDefinition.xsd",
+			"generator.xsd",
+		)
+		for schemaFile in schemaFiles:
+			_ = XMLSchema(directory / schemaFile)
